67e39986e3ba28c98a962f7fd02059de9b91d02a
Ian Thomas Dover <ianthomasdover@Mac.attlocal.net>
2025-07-26 19:06:10 -0500

    fd it up

--- collection_viewer/authentication.py
+++ collection_viewer/authentication.py
@@ -1,53 +1,51 @@
-from urllib.parse import unquote_plus
 import streamlit as st
-import os
+from urllib.parse import unquote_plus
 
-from dotenv import load_dotenv
-
-load_dotenv()  # if you’re using a .env file
-
-DJANGO_LOGOUT_URL = os.getenv(
-    "DJANGO_LOGOUT_URL",
-    "http://localhost:8000/oauth/logout/"
-)
+DJANGO_LOGIN_URL  = "http://localhost:8000/oauth/login/"
+DJANGO_LOGOUT_URL = "http://localhost:8000/oauth/logout/"
 
 def login():
+    # 1) Wipe any stale query-params before reading them
     qp = st.query_params
 
-    # 1) If we see the callback params, bootstrap session_state.user
+    # 2) If we’ve just come back from Django, grab email+name
     if "django_auth" in qp and "user" not in st.session_state:
         email = qp.get("email", [""])
-        name = qp.get("name",  [""])
+        name  = qp.get("name",  [""])
         if email:
-            # decode in case of URL-encoding
             st.session_state.user = {
                 "email": unquote_plus(email),
                 "name":  unquote_plus(name),
             }
 
-    # 2) If still not logged in, send them to Django’s login
+    # 3) If still not logged in, redirect *top* window to Django’s /oauth/login/
     if "user" not in st.session_state:
-        js = """
-        <script>
-          window.location.href = "http://localhost:8000/oauth/login/";
-        </script>
-        """
-        st.components.v1.html(js, heisght=0)
+        st.components.v1.html(
+            f"""
+            <script>
+              window.top.location.href = "{DJANGO_LOGIN_URL}";
+            </script>
+            """,
+            height=0,
+        )
         st.stop()
 
     return st.session_state.user["email"]
 
+
 def logout_button():
     if st.button("🔒 Log out"):
-        # 1) Clear Streamlit’s session_state
-        for key in list(st.session_state.keys()):
-            del st.session_state[key]
+        # 1) Clear all Streamlit state
+        for k in list(st.session_state.keys()):
+            del st.session_state[k]
 
-        # 2) Redirect browser to Django’s logout
-        js = f"""
-        <script>
-          window.location.href = "{DJANGO_LOGOUT_URL}";
-        </script>
-        """
-        st.components.v1.html(js, height=0)
+        # 3) Redirect top window to Django’s logout endpoint
+        st.components.v1.html(
+            f"""
+            <script>
+              window.top.location.href = "{DJANGO_LOGOUT_URL}";
+            </script>
+            """,
+            height=0,
+        )
         st.stop()
